b2563fb00774d34d84fe79f28b4c57b731bc3595
R.A.D.H.A.Samaranayake <168948385+HiranSamaranayake@users.noreply.github.com>
2026-06-16 10:59:34 +0530

    Added React routing setup

--- eventease-frontend/src/App.js
+++ eventease-frontend/src/App.js
@@ -1,9 +1,52 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+
+import MyBookings from './pages/customer/MyBookings';
+
+import OrganizerDashboard from './pages/organizer/Dashboard';
+
+import AdminDashboard from './pages/admin/Dashboard';
+
 function App() {
-  return (
-    <div>
-      <h1>EventEase Frontend Started 🚀</h1>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+
+                <Route
+                    path="/"
+                    element={<h1>EventEase Home</h1>}
+                />
+
+                <Route
+                    path="/login"
+                    element={<Login />}
+                />
+
+                <Route
+                    path="/register"
+                    element={<Register />}
+                />
+
+                <Route
+                    path="/customer/bookings"
+                    element={<MyBookings />}
+                />
+
+                <Route
+                    path="/organizer/dashboard"
+                    element={<OrganizerDashboard />}
+                />
+
+                <Route
+                    path="/admin/dashboard"
+                    element={<AdminDashboard />}
+                />
+
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ eventease-frontend/src/pages/customer/MyBookings.js
@@ -0,0 +1,5 @@
+function MyBookings() {
+    return <h2>My Bookings Page</h2>;
+}
+
+export default MyBookings;
