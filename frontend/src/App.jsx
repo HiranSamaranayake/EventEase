b8fbfde97ce8ba2f6401b6a6631bd04faa033d4e
R.A.D.H.A.Samaranayake <168948385+HiranSamaranayake@users.noreply.github.com>
2026-06-16 17:34:29 +0530

    Created React folder structure and routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,7 @@
-function App() {
-  return (
-    <div className="min-h-screen bg-white flex items-center justify-center">
-      <div className="bg-purple-700 text-white px-10 py-6 rounded-2xl shadow-xl">
-        <h1 className="text-4xl font-bold">
-          Welcome to EventEase
-        </h1>
+import AppRoutes from "./routes/AppRoutes";
 
-        <p className="mt-3 text-center">
-          Purple • White • Black Theme Activated 🚀
-        </p>
-      </div>
-    </div>
-  )
+function App() {
+    return <AppRoutes />;
 }
 
 export default App;
