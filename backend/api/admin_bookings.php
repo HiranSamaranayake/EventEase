ec2ad7f8bab14a6b50998abb090b7205a33e4aae
hashenhewage <164984059+hashenhewage@users.noreply.github.com>
2026-07-13 00:50:41 +0530

    Admin booking error fix

--- backend/api/admin_bookings.php
+++ backend/api/admin_bookings.php
@@ -1,8 +1,11 @@
 <?php
 
-require_once "cors.php";
-require_once "../config/database.php";
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Content-Type: application/json");
 
+require_once "../config/database.php";
 
 
 $sql="
