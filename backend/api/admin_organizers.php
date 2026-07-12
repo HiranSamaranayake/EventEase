5033d3ee0adf3d4e86914a97770c77a2c8cb242a
hashenhewage <164984059+hashenhewage@users.noreply.github.com>
2026-07-13 01:34:47 +0530

    error fix

--- backend/api/admin_organizers.php
+++ backend/api/admin_organizers.php
@@ -1,9 +1,15 @@
 <?php
 
-header("Access-Control-Allow-Origin: http://localhost:5175");
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Content-Type: application/json");
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    exit;
+}
+
 
 require_once "../config/database.php";
 
